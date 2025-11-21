92c428da572444daaff63f19686fe8a27729709e
GarethNobleLFG <157191136+GarethNobleLFG@users.noreply.github.com>
2025-11-20 21:52:55 -0500

    Login Features + Card And Decks Save To Current User + Render Content On Page Access (#15)
    * Added Working Sign-Up And Login Service. Updated How Cards Are Saved Relative To User.
    
    * Decks and Cards Now Saving to Database.
    
    * Fixed a Few Bugs, Edit Card Properly Works, Decks + Cards Save To Database.
    
    * Removed Some De-Bug Lines.
    
    * Decks Re-Render Upon Creation.
    
    * Added env Variable For GPT Version, Took Commented Code Out, Fixed Bug With Card Mapping On AI Tool.

--- backend/models/Deck.js
+++ backend/models/Deck.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const deckSchema = new mongoose.Schema({
     deckID: {type: Number, required: true, unique: true},
-    userID: {type: Number, required: true},
+    userEmail: {type: String, required: true} ,
     title: {type: String, required: true}
 });
 
