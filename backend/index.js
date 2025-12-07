ffb51a3ef8e47d2cf332b632654cb382eb422dd7
Bo Wang <wangb02@pfw.edu>
2025-12-07 12:32:44 -0500

    configurating unit testing; create unit testing for user creating (#21)

--- backend/index.js
+++ backend/index.js
@@ -82,7 +82,7 @@ app.get('/', (req, res) => {
 
 
 // For local development only
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
   connectToDatabase().then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
